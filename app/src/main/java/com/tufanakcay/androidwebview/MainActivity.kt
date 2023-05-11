17db990f363767c49903e4c8b3d9ff0df51a251c
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-12 00:01:17 +0300

    Update MainActivity.kt

--- app/src/main/java/com/tufanakcay/androidwebview/MainActivity.kt
+++ app/src/main/java/com/tufanakcay/androidwebview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.tufanakcay.androidwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
     }
 }
